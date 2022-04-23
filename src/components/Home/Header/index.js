9f9fb47faef3cdc5ef75c68402b273c52c0342f2
nyoman <nyeka2101@gmail.com>
2022-04-23 16:04:35 +0700

    make a blog website

---
+++ src/components/Home/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+const Header = () => (
+  <header className="home-header">
+    <h2>Nyoman Eka Swardita</h2>
+    <h1>
+      <span>“</span> Blog <span>”</span>
+    </h1>
+    <p>
+      awesome place to make oneself <br /> productive and entertained through
+      daily updates.
+    </p>
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,15 @@
+export const blogList = [
+  {
+    id: 1,
+    title: "Jasa Joki Algortihma Pemrograman ",
+    category: "development",
+    subCategory: ["Joki", "Algorithma", "C++"],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: "Nyoman Ganteng",
+    authorAvatar: "assets/images/author.jpeg",
+    createdAt: "June 03, 2021",
+    link: "assets/tata/data.html",
+    cover: "assets/testing.png",
+  },
+];
